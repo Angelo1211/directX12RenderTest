9628d46c2d8f43106cdcfc7fee33a4d526f48981
angel <angelo1211github.com>
2019-02-23 13:27:50 +0100

    First Hello World + build and run settings

--- DirectX12RenderDemo/main.cpp
+++ DirectX12RenderDemo/main.cpp
@@ -1,1 +1,17 @@
 #include <windows.h>
+
+/*
+    Main entry point for windows functions:
+    The goal is to initialize the application, display the main window and enter message retrieval
+*/
+int WinMain(
+  HINSTANCE hInstance,     //Handle to current program
+  HINSTANCE hPrevInstance, //Handle to previous program instance? If you want to detect if another exists
+  LPSTR     lpCmdLine ,    //Command line for program
+  int       nShowCmd       //No idea what this does seems to always be  10
+)
+{
+
+    OutputDebugStringA("Hello World!");
+    return 0;
+}
